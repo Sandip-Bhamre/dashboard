f425ee98b9d45a4516bc92741428bb99b0b5e810
sandip_bhamre <shamvns987@gmail.com>
2024-01-29 01:07:26 +0530

    commit

--- src/app/typography/typography.component.ts
+++ src/app/typography/typography.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit  } from '@angular/core';
+import { DomSanitizer, SafeUrl , SafeResourceUrl  } from '@angular/platform-browser';
+// import { Component, DomSanitizer, SafeUrl } from '@angular/core';
 @Component({
   selector: 'app-typography',
   templateUrl: './typography.component.html',
   styleUrls: ['./typography.component.css']
 })
-export class TypographyComponent implements OnInit {
+export class TypographyComponent  {
+   youtubeVideoUrls = [
+   { url: 'https://www.youtube.com/watch?v=tThJdNt-_5I&list=RDtThJdNt-_5I&start_radio=1', description: 'Laal Peeli Akhiyan'},
+   { url :'https://www.youtube.com/watch?v=GvXDq-P1NB8&list=RDtThJdNt-_5I&index=2', description : 'Akhiyaan Gulaab'},
+   { url :'https://www.youtube.com/watch?v=Vhm4mTh_UvU&list=RDtThJdNt-_5I&index=6', description : 'Sajda Karaan'},
+   { url :'https://www.youtube.com/watch?v=Ja168gMpb3o&list=RDtThJdNt-_5I&index=5', description : 'Why this Kolaveri Di'},
+   { url :'https://www.youtube.com/watch?v=RIz96zpbeBI&list=RDtThJdNt-_5I&index=8', description : 'Haiwaan'},
+  ]; 
 
-  constructor() { }
+  selectedVideo: SafeUrl | null = null;
 
-  ngOnInit() {
+  constructor(private sanitizer: DomSanitizer) {}
+
+  ngOnInit(): void { }
+
+  getEmbeddedUrl(youtubeUrl: string): SafeResourceUrl {
+    const videoId = this.getVideoId(youtubeUrl);
+    const embeddedUrl = `https://www.youtube.com/embed/${videoId}`;
+    return this.sanitizer.bypassSecurityTrustResourceUrl(embeddedUrl);
   }
 
+  private getVideoId(youtubeUrl: string): string {
+    const videoIdMatch = youtubeUrl.match(/(?:youtu\.be\/|youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/);
+    return videoIdMatch ? videoIdMatch[1] : '';
+  }
+
+  onFileSelected(event: any): void {
+    const file = event.target.files[0];
+
+    if (file) {
+      const blobUrl = URL.createObjectURL(file);
+      this.selectedVideo = this.sanitizer.bypassSecurityTrustUrl(blobUrl);
+    }
+  }
 }
